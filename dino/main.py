5cdfbb152b5d2366b533872b9c41ddd0949a7dc6
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2022-12-23 14:20:24 +0100

    1.2
    + dino can crawl
    + 3 different height birds can spawn
    + 3 different textures for birds

--- dino/main.py
+++ dino/main.py
@@ -12,24 +12,34 @@ class Dino(pygame.sprite.Sprite):
         super().__init__()
         run1 = pygame.image.load('images/dino_run1.xcf').convert_alpha()
         run2 = pygame.image.load('images/dino_run2.xcf').convert_alpha()
-        self.stand = pygame.image.load('images/1-night.png').convert_alpha()
+        crawl1 = pygame.image.load('images/default/dino_crawl.png').convert_alpha()
+        crawl2 = pygame.image.load('images/dino_crawl2.xcf').convert_alpha()
+        self.stand = pygame.image.load('images/default/1-night.png').convert_alpha()
         self.frames = [run1, run2]
+        self.crawl_frames = [crawl1, crawl2]
         self.frame_index = 0
 
         self.jump_sound = pygame.mixer.Sound('sounds/jump_mario.wav')
         self.jump_sound.set_volume(0.5)
 
         self.image = self.frames[self.frame_index]
-        self.rect = self.image.get_rect(midbottom=(100, 525))
+        self.rect = self.image.get_rect(midbottom=(200, 525))
+        
+        self.isCrawling = False
 
         self.gravity = 0
 
     def player_input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_SPACE] and self.rect.bottom == 525:
+        if keys[pygame.K_SPACE] and self.rect.bottom == 525 and not self.isCrawling:
             self.gravity = -22
             self.jump_sound.play()
+            self.isCrawling = False
+        elif keys[pygame.K_s] and self.rect.bottom == 525:
+            self.isCrawling = True
+        else:
+            self.isCrawling = False
 
     def apply_gravity(self):
         self.gravity += 1
@@ -46,7 +56,13 @@ class Dino(pygame.sprite.Sprite):
             self.frame_index += 0.2
             if self.frame_index >= len(self.frames):
                 self.frame_index = 0
-            self.image = self.frames[int(self.frame_index)]
+
+            if self.isCrawling:
+                self.image = self.crawl_frames[int(self.frame_index)]
+            else:
+                self.image = self.frames[int(self.frame_index)]
+
+            self.rect = self.image.get_rect(midbottom=(200, 525))
 
     def update(self):
         self.player_input()
@@ -58,13 +74,26 @@ class Obstacles(pygame.sprite.Sprite):
     def __init__(self, type):
         super().__init__()
 
+        # images
+        cactus = pygame.image.load('images/default/4-green.png').convert_alpha()
+        red_bird = pygame.image.load('images/bird.xcf').convert_alpha()
+        purple_bird = pygame.image.load('images/bird_purple.xcf').convert_alpha()
+        yellow_bird = pygame.image.load('images/bird_yellow.xcf').convert_alpha()
+
         if type == 'Cactus':
-            self.image = pygame.image.load('images/4-green.png').convert_alpha()
+            self.image = cactus
             y_position = 525
             self.speed = 10
         else:
-            self.image = pygame.image.load('images/bird.xcf').convert_alpha()
-            y_position = 375
+            if type == 'bird_low':
+                y_position = 525
+            elif type == 'bird_mid':
+                y_position = 425
+            elif type == 'bird_high':
+                y_position = 350
+            else:
+                y_position = 300
+            self.image = choice([red_bird, purple_bird, yellow_bird])
             self.speed = 10
 
         self.rect = self.image.get_rect(midbottom=(randint(1400, 1600), y_position))
@@ -92,7 +121,7 @@ clock = pygame.time.Clock()
 # intro surfaces
 background = pygame.image.load('images/sky.png').convert()
 background = pygame.transform.scale(background, (1280, 720))
-dino_stand = pygame.image.load('images/1-night.png').convert_alpha()
+dino_stand = pygame.image.load('images/default/1-night.png').convert_alpha()
 dino_stand_rectangle = dino_stand.get_rect(center=(640, 350))
 game_title = font.render('DINO', False, 'Black')
 game_title_rectangle = game_title.get_rect(center=(640, 175))
@@ -113,15 +142,17 @@ text_outro_rectangle = text_outro.get_rect(center=(640, 500))
 
 # timer
 obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer, 1700)
+pygame.time.set_timer(obstacle_timer, 1800)
 cloud_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(cloud_timer, 1000)
 
 # music
 background_music = pygame.mixer.Sound('music/background_mario.mp3')
 background_music.set_volume(0.25)
+
 # sounds
 game_over = pygame.mixer.Sound('sounds/die.wav')
+
 # sprite groups
 dino = pygame.sprite.GroupSingle()
 dino.add(Dino())
@@ -131,11 +162,11 @@ obstacle_group = pygame.sprite.Group()
 def cloud_generator(cloud_list):
     if cloud_list:
         for cloud_rect in cloud_list:
-            cloud_rect.x -= 2
+            cloud_rect.x -= 1
 
             screen.blit(cloud_surface, cloud_rect)
 
-        obstacle_list = [cloud for cloud in cloud_list if cloud.x > -100]
+        cloud_list = [cloud for cloud in cloud_list if cloud.x > -100]
 
         return cloud_list
 
@@ -186,10 +217,10 @@ while True:
                 background_music.play(loops=-1)
 
         if event.type == obstacle_timer and game_active:
-            obstacle_group.add(Obstacles(choice(['Cactus', 'Cactus', 'Cactus', 'Bird'])))
+            obstacle_group.add(Obstacles(choice(['Cactus', 'Cactus', 'Cactus', choice(['bird_high', 'bird_low', 'bird_mid'])])))
 
         if event.type == cloud_timer:
-            cloud_rect_list.append(cloud_surface.get_rect(bottomright=(randint(1400, 1600), randint(75, 125))))
+            cloud_rect_list.append(cloud_surface.get_rect(bottomright=(randint(1400, 1600), randint(75, 175))))
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
